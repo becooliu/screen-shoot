93030c235e41666bcd6c420c1ef2bbc39dfa3da5
becooliu <243257686@qq.com>
2025-09-28 18:48:49 +0800

    优化消息监听处理

--- src/content/content-script.js
+++ src/content/content-script.js
@@ -5,10 +5,22 @@ const panel = new ExtensionPanel();
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("listen from service worker");
-  if (message.type === "togglePanel") {
+  /* if (message.type === "togglePanel") {
     panel.checkUrlHasConfig();
     panel.changeStatus();
     panel.toggle();
     sendResponse(true);
+  } */
+
+  switch (message.type) {
+    case "togglePanel":
+      panel.checkUrlHasConfig();
+      panel.changeStatus();
+      panel.toggle();
+      sendResponse(true);
+      break;
+
+    default:
+      break;
   }
 });
